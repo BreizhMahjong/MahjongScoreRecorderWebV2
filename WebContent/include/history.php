3a130771fa2da351d27e2ad4f13cf1ed18cb0ba4
zylqf <zyl_qf@yahoo.com>
2018-08-15 12:26:43 +0200

    Default period mode semester on all page

--- WebContent/include/history.php
+++ WebContent/include/history.php
@@ -8,18 +8,19 @@
 			</select></td>
 			<td>Mois&nbsp;:</td>
 			<td><select id="selectMonth">
-					<option value="0">Janvier</option>
-					<option value="1">Février</option>
-					<option value="2">Mars</option>
-					<option value="3">Avril</option>
-					<option value="4">Mai</option>
-					<option value="5">Juin</option>
-					<option value="6">Juillet</option>
-					<option value="7">Août</option>
-					<option value="8">Septembre</option>
-					<option value="9">Octobre</option>
-					<option value="10">Novembre</option>
-					<option value="11">Décembre</option>
+					<?php $month = intval (date ("m")) - 1; ?>
+					<option value="0" <?php if ($month == 0) { echo " selected=\"selected\""; }?>>Janvier</option>
+					<option value="1" <?php	if ($month == 1) { echo " selected=\"selected\""; }?>>Février</option>
+					<option value="2" <?php if ($month == 2) { echo " selected=\"selected\""; }?>>Mars</option>
+					<option value="3" <?php if ($month == 3) { echo " selected=\"selected\""; }?>>Avril</option>
+					<option value="4" <?php if ($month == 4) { echo " selected=\"selected\""; }?>>Mai</option>
+					<option value="5" <?php if ($month == 5) { echo " selected=\"selected\""; }?>>Juin</option>
+					<option value="6" <?php if ($month == 6) { echo " selected=\"selected\""; }?>>Juillet</option>
+					<option value="7" <?php if ($month == 7) { echo " selected=\"selected\""; }?>>Août</option>
+					<option value="8" <?php if ($month == 8) { echo " selected=\"selected\""; }?>>Septembre</option>
+					<option value="9" <?php if ($month == 9) { echo " selected=\"selected\""; }?>>Octobre</option>
+					<option value="10" <?php if ($month == 10) { echo " selected=\"selected\""; }?>>Novembre</option>
+					<option value="11" <?php if ($month == 11) { echo " selected=\"selected\""; }?>>Décembre</option>
 			</select></td>
 			<td>Jour&nbsp;:</td>
 			<td><select id="selectDay">
--- WebContent/include/ranking.php
+++ WebContent/include/ranking.php
@@ -33,7 +33,7 @@
 		</select></td>
 		<td style="text-align: right; width: 10%;">Trimestre&nbsp;:</td>
 		<td style="width: 10%;"><select id="selectTrimester" style="width: 100%;">
-				<?php $trimester = floor((intval(date("m"))-1)/3); ?>
+				<?php $trimester = floor ((intval (date ("m")) - 1) / 3);?>
 				<option value="0" <?php if($trimester==0) { echo " selected=\"selected\""; } ?>>1er</option>
 				<option value="1" <?php if($trimester==1) { echo " selected=\"selected\""; } ?>>2ème</option>
 				<option value="2" <?php if($trimester==2) { echo " selected=\"selected\""; } ?>>3ème</option>
